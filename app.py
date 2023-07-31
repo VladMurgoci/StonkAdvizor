0cd3cd0b005833aeef5dd23b731432ba7bdde2e4
vmurgoci <v.murgoci@student.tudelft.nl>
2023-08-01 02:53:54 +0300

    Load stocks to front-end and create tidy class for individual stock info

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from metrics.fundamental_analysis import get_ticker_symbols_finnhub
+from metrics.fundamental_analysis import get_fundamental_analysis_metrics
+from metrics.utils.stock_metrics import StockMetrics
+from flask import jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def stock_browser():
+    # Sample data for testing the layout
+    tickers = get_ticker_symbols_finnhub()
+    return render_template('stock_browser.html', tickers=tickers)
+
+
+@app.route('/stock/<ticker_symbol>')
+def get_stock_data(ticker_symbol):
+    metrics = StockMetrics(ticker_symbol)
+    return jsonify(metrics)
+
+
+@app.route('/tickers')
+def get_tickers():
+    tickers = get_ticker_symbols_finnhub()
+    return tickers[0:10]
+    #return tickers
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ metrics/utils/stock_metrics.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+import finnhub
+import yfinance as yf
+
+FINNHUB_CLIENT = finnhub.Client(api_key="cj2o6rhr01qr0f6fq0k0cj2o6rhr01qr0f6fq0kg")
+EXCHANGES = ['US']
+
+@dataclass
+class StockMetrics:
+    """
+        Metrics for one stock
+    """
+
+    ticker_symbol: str
+    market_cap: Optional[int] = None
+    activity_domain: Optional[str] = None
+    net_income_4yrs: Optional[List[float]] = None
+    net_income: Optional[float] = None
+    basic_eps_4yrs: Optional[List[float]] = None
+    basic_eps: Optional[float] = None
+
+
+    def __init__(self, ticker_symbol: str):
+        """
+        Initiliazes stock metrics based on yahoo finance query
+        :return: StockMetrics
+        """
+        self.ticker_symbol = ticker_symbol
+        ticker = yf.Ticker(ticker_symbol)
+        ticker_info = ticker.info
+        yearly_income_statement, yearly_balance_sheet  = ticker.income_stmt, ticker.quarterly_balance_sheet
+        financials = ticker.financials
+        self._initialize_from_ticker_info(ticker_info)
+        self._initialize_from_yearly_income_statement(yearly_income_statement)
+        self._initialize_from_yearly_balance_sheet(yearly_balance_sheet)
+        self.initialize_from_financials(financials)
+
+    def _initialize_from_ticker_info(self, ticker_info):
+        if 'marketCap' in ticker_info:
+            self.market_cap = ticker_info['marketCap']
+        if 'activity_domain' in ticker_info:
+            self.activity_domain = ticker_info['actviity_domain']
+
+    def _initialize_from_yearly_income_statement(self, yearly_income_statement):
+        if 'Basic EPS' in yearly_income_statement.index:
+            self.earnings_per_share = yearly_income_statement.loc['Basic EPS'].tolist()
+            self.earnings_per_share = self.earnings_per_share[0]
+
+
+    def _initialize_from_yearly_balance_sheet(self, yearly_balance_sheet):
+        pass
+
+    def initialize_from_financials(self, financials):
+        if 'Total Revenue' in financials.index:
+            self.net_income_4yrs = financials.loc['Total Revenue'].tolist()
+            self.net_income = self.net_income_4yrs[0]
+
